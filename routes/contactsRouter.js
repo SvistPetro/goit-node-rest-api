34cabe23093f10e25ec15b51f9b4b0350ec4a543
Petro Svist <svist.petro@gmail.com>
2024-05-16 23:16:32 +0300

    Final HW 3

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,22 +1,27 @@
 import express from "express";
+
 import {
   getAllContacts,
   getOneContact,
   deleteContact,
   createContact,
   updateContact,
+  updateStatusContact
 } from "../controllers/contactsControllers.js";
 
 const contactsRouter = express.Router();
+const jsonParser = express.json();
 
 contactsRouter.get("/", getAllContacts);
 
 contactsRouter.get("/:id", getOneContact);
 
+contactsRouter.post("/", jsonParser, createContact);
+
+contactsRouter.put("/:id", jsonParser, updateContact);
+
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
-
-contactsRouter.put("/:id", updateContact);
+contactsRouter.patch("/:id/favorite", jsonParser, updateStatusContact);
 
 export default contactsRouter;
